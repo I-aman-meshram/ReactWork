edde6906c1cb5cd6e2f4475c58a999d5eff678fa
Aman Meshram <131248832+I-aman-meshram@users.noreply.github.com>
2024-01-11 20:51:37 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Resturant from './component/Basics/Resturant'
+import UseEffect from './component/Hooks/useEffect';
+import UseReducer from './component/Hooks/useReducer';
+
+const App = () => {
+  // return <Resturant/>
+ //return <UseEffect/>
+ return <UseReducer/> 
+
+};
+
+
+
+export default App
